885475747515bc72950423465114850e598a0afb
meenakshi930 <meenakshi.gupta_cs.aiml24@gla.ac.in>
2026-02-20 09:27:05 +0530

    configure analog input pin for pressure sensor

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,8 @@
 
 int pressureValue = 0;
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Print system initialization message
+    Serial.begin(9600);
+    pinMode(A0, INPUT);
 }
 
 void loop() {
